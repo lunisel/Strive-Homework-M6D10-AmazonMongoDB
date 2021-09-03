1f9bcba5d7f5ae08ea24ab564f69240ab55fa9f6
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-03 13:00:15 +0200

    change

--- src/service/cart/index.js
+++ src/service/cart/index.js
@@ -2,18 +2,21 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const cartSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  products: [
-    {
-      name: String,
-      brand: String,
-      imageUrl: String,
-      price: Number,
-      quantity: Number,
-    },
-  ],
-  status: { type: String, enum: ["unpaid", "paid"] },
-});
+const cartSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    products: [
+      {
+        name: String,
+        brand: String,
+        imageUrl: String,
+        price: Number,
+        quantity: Number,
+      },
+    ],
+    status: { type: String, enum: ["unpaid", "paid"] },
+  },
+  { timestamps: true }
+);
 
 export default model("Cart", cartSchema);
