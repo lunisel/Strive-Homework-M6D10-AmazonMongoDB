5214cf4dec61e64f57f63698f368da981dad235f
Luna Sarmiento <selene942008@hotmail.com>
2021-09-03 15:49:23 +0200

    changes

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,82 +1,81 @@
-import express from "express"
-import createError from "http-errors"
+import express from "express";
+import createError from "http-errors";
 
-import {ProductModel} from "./schema.js"
+import { ProductModel } from "./schema.js";
 
-const productRouter = express.Router()
+const productRouter = express.Router();
 
-productRouter.post('/', async (req, res, next)=> {
-    try {
-        const newProduct = new ProductModel(req.body)
-        const {_id} = await newProduct.save()
+productRouter.post("/", async (req, res, next) => {
+  try {
+    const newProduct = new ProductModel(req.body);
+    const { _id } = await newProduct.save();
 
-        res.status(201).send(`My unique id: ${_id}`)
-    } catch (error) {
-        next(error)
+    res.status(201).send(`My unique id: ${_id}`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+productRouter.get("/", async (req, res, next) => {
+  try {
+    const products = await ProductModel.find({});
+
+    res.send(products);
+  } catch (error) {
+    next(error);
+  }
+});
+
+productRouter.get("/:productId", async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
+
+    const product = await ProductModel.findById(productId); // similar to findOne()
+
+    if (product) {
+      res.send(product);
     }
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
-productRouter.get("/", async(req,res,next) => {
-    try {
-      
-      const products = await ProductModel.find({}).populate("author")
-  
-      res.send(products)
-      
-    } catch (error) {
-      next(error)
-    }
-  })
+productRouter.put("/:productId", async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
 
-  productRouter.get("/:productId", async(req,res,next) => {
-    try {
-      
-      const productId = req.params.productId
-
-    const product = await ProductModel.findById(productId) // similar to findOne()
-
-    if(product){
-
-      res.send(product)
-    }
-      
-    } catch (error) {
-      next(error)
-    }
-  })
-
-  productRouter.put("/:productId", async(req,res,next) => {
-    try {
-      const productId = req.params.productId
-  
-      const modifiedproduct = await ProductModel.findByIdAndUpdate(productId, req.body, {
-        new: true // returns the modified product
-      })
-  
-      if(modifiedproduct){
-        res.send(modifiedproduct)
-      } else {
-        next(createError(404, `product with id ${productId} not found!`))
+    const modifiedproduct = await ProductModel.findByIdAndUpdate(
+      productId,
+      req.body,
+      {
+        new: true, // returns the modified product
       }
-    } catch (error) {
-      next(error)
-    }
-  })
+    );
 
-  productRouter.delete("/:productId", async(req,res,next) => {
-    try {
-      const productId = req.params.productId
-  
-      const deletedproduct = await ProductModel.findByIdAndDelete(productId)
-  
-      if(deletedproduct){
-        res.status(204).send()
-      } else {
-        next(createError(404, `product with id ${productId} not found!`))
-      }
-    } catch (error) {
-      next(error)
+    if (modifiedproduct) {
+      res.send(modifiedproduct);
+    } else {
+      next(createError(404, `product with id ${productId} not found!`));
     }
-  })
+  } catch (error) {
+    next(error);
+  }
+});
 
-export default productRouter
+productRouter.delete("/:productId", async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
+
+    const deletedproduct = await ProductModel.findByIdAndDelete(productId);
+
+    if (deletedproduct) {
+      res.status(204).send();
+    } else {
+      next(createError(404, `product with id ${productId} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default productRouter;
--- src/services/products/schema.js
+++ src/services/products/schema.js
@@ -7,7 +7,7 @@ const productSchema = new Schema(
     name: { type: String, required: true },
     description: { type: String, required: true },
     brand: { type: String, required: true },
-    imageUrl: { type: String, required: true },
+    imageUrl: { type: String /* , required: true  */ },
     price: { type: Number, required: true },
     category: { type: String, required: true },
   },
