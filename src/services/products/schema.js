ecc6539a1bf2f3fba82855d757121f1affa97012
Sarath-97 <sarath559709@gmail.com>
2021-09-03 16:33:36 +0100

    changes

--- src/services/products/schema.js
+++ src/services/products/schema.js
@@ -9,7 +9,6 @@ const productSchema = new Schema(
     brand: { type: String, required: true },
     imageUrl: { type: String /* , required: true  */ },
     price: { type: Number, required: true },
-    category:{type: Schema.Types.ObjectId, ref: true},
     reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }]
 
   },
