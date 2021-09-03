eb5e0fa021851591855bd4aae8b97ef6e450d431
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-03 14:56:04 +0200

    cart

--- src/server.js
+++ src/server.js
@@ -1,27 +1,35 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import productRouter from "./services/products/index.js";
 import listEndpoints from "express-list-endpoints";
+import reviewRouter from "./reviews/index.js";
 import cartRouter from "./service/cart/index.js";
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 
+/* **************ROUTES ***************** */
+
+server.use("/reviews", reviewRouter);
+server.use("/products", productRouter);
 server.use("/carts", cartRouter);
 
-mongoose.connect(process.env.MONGO_CON);
-mongoose.connection.on(`connected`, () => {
-  console.log(`🥭Connected to 🥭 Successfully`);
+/* ***************CONNECTION TO MONGO COMPASS */
+mongoose.connect(process.env.MONGO_CONNECTION);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connection Successful to mongo!");
   server.listen(port, () => {
     console.table(listEndpoints(server));
-    console.log(`🖥💻 runing on: ${port}`);
+    console.log(`Server is running on port ${port}`);
   });
 });
 
-mongoose.connection.on(`error`, (err) => {
-  console.log(`error==> ${err}`);
+mongoose.connection.on("error", (err) => {
+  console.log("MONGO ERROR ", err);
 });
